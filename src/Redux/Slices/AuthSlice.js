65ec9e2628322238766eecff669075868cd35ae0
Sagar <sagarsurya45@gmail.com>
2025-04-08 12:35:55 +0530

    signup page

---
+++ src/Redux/Slices/AuthSlice.js
@@ -0,0 +1,38 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import toast from "react-hot-toast";
+import axiosInstance from "../../Helpers/axiosInstance"
+
+const initialState = {
+
+    isLoggedIn: localStorage.getItem("isLoggedIn") || false,
+    role: localStorage.getItem("role")  || "",
+    data: localStorage.getItem('data') || {}
+
+}
+
+export const createAccount = createAsyncThunk("auth/signup", async (data) => {
+    try {
+
+        const response = axiosInstance.post("/user/register", data);
+            toast.promise(response, {
+                loading:"Wait !creating your account",
+                success:(data) => {return data?. data?.message},
+                error:"failed to create account"
+            })
+            return (await response).data;        
+    } catch (error) {
+        toast.error(error?.reponse?.data?.message)
+    }
+})
+
+// this is a async thunk , it's work like action, but action first ot will resloce it's promise and then action dispatch 
+
+const AuthSlice = createSlice({
+    name:'authSlicer',
+    initialState,
+    reducers:{}
+
+})
+
+export const {}  = AuthSlice.actions;
+export default AuthSlice.reducer
